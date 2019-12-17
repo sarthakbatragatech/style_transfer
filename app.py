8c53a80e113328232a5a97139ad6e0bcf98c4826
Sarthak Batra <sarthakbatra1993@gmail.com>
2019-12-17 20:37:33 +0530

    Using send_from_directory to display uploaded images

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
 from pathlib import Path
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
-dir_path = Path('.')
-upload_path = dir_path/'static'
-app.config['UPLOAD_FOLDER'] = str(upload_path)
 
-@app.route('/', methods=['GET', 'POST'])
+# This will give us the PosixPath of the working directory
+dir_path = Path('.')
+upload_path = str(dir_path/'static'/'uploads')
+
+# If we land on the homepage
+@app.route('/')
 def home():
-    if request.method=='GET':
-        return render_template('index.html')
-    
+    return render_template('index.html')
+
+# Uploading Style and Content Images
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
     if request.method=='POST':
         content_image = request.files['content']
         style_image = request.files['style']
-        content_image.save(str(upload_path/content_image.filename))
-        style_image.save(str(upload_path/style_image.filename))
+        content_image.save(upload_path + '/' + content_image.filename)
+        style_image.save(upload_path + '/' + style_image.filename)
         return render_template(
             'result.html', 
             content=content_image.filename,
             style=style_image.filename)
+
+# Function to help display the uploaded files
+@app.route('/upload/<filename>')
+def serve_image(filename):
+    return send_from_directory(
+        directory='static/uploads',
+        filename=filename)
