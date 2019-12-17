987c67b882992e89c8fe28ca02e67fa7baa5df48
Sarthak Batra <sarthakbatra1993@gmail.com>
2019-12-17 15:06:29 +0530

    Display uploaded images

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+from pathlib import Path
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
+dir_path = Path('.')
+upload_path = dir_path/'static'
+app.config['UPLOAD_FOLDER'] = str(upload_path)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -9,6 +14,8 @@ def home():
     if request.method=='POST':
         content_image = request.files['content']
         style_image = request.files['style']
+        content_image.save(str(upload_path/content_image.filename))
+        style_image.save(str(upload_path/style_image.filename))
         return render_template(
             'result.html', 
             content=content_image.filename,
