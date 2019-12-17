47e4873ff69d2a9db23adcd82808b9d879391652
Sarthak Batra <sarthakbatra1993@gmail.com>
2019-12-17 12:41:50 +0530

    Successfully uploaded images on flask server

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 def home():
     if request.method=='GET':
         return render_template('index.html')
+    
+    if request.method=='POST':
+        content_image = request.files['content']
+        style_image = request.files['style']
+        return render_template(
+            'result.html', 
+            content=content_image.filename,
+            style=style_image.filename)
