71d91b520bb841e64054f6a475cab0c3e29e3c37
Sarthak Batra <sarthakbatra1993@gmail.com>
2019-12-18 13:32:12 +0530

    Accept only valid file extentions for uploads

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from pathlib import Path
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, redirect
+# from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+def is_file_allowed(filename):
+    if not "." in filename:
+        return False
+
+    suffix = filename.rsplit('.', 1)[1]
+
+    if suffix.lower() in ['jpeg', 'jpg', 'png']:
+        return True
+    else:
+        return False
+
 # This will give us the PosixPath of the working directory
 dir_path = Path('.')
+# Using it we create a string path for the upload folder
 upload_path = str(dir_path/'static'/'uploads')
 
 # If we land on the homepage
@@ -12,26 +25,36 @@ upload_path = str(dir_path/'static'/'uploads')
 def home():
     return render_template('index.html')
 
-# Uploading Style and Content Images
+# Deal with uploaded Style and Content Images
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method=='POST':
         content_image = request.files['content']
         style_image = request.files['style']
-        content_image.save(upload_path + '/' + content_image.filename)
-        style_image.save(upload_path + '/' + style_image.filename)
+        content_filename = content_image.filename
+        style_filename = style_image.filename
+        if is_file_allowed(content_filename):
+            content_image.save(
+                upload_path + '/' + content_filename)
+        if is_file_allowed(style_filename):
+            style_image.save(
+                upload_path + '/' + style_filename)
+        else:
+            print("Check the file extention you've uploaded")
+            return render_template('index.html')
         return render_template(
             'upload.html', 
             content=content_image.filename,
             style=style_image.filename)
 
-# Function to help display the uploaded files
+# Serve the uploaded files
 @app.route('/upload/<filename>')
 def serve_image(filename):
     return send_from_directory(
         directory='static/uploads',
         filename=filename)
 
+# Route where style transfer results will be displayed
 @app.route('/model', methods=['GET', 'POST'])
 def style_transfer():
     if request.method=='POST':
